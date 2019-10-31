09db2c0db469879cb73673d6c2f49cf5de6b4c83
Edwin Pan <edwin.h.pan@gmail.com>
2019-10-31 01:36:32 -0500

    this is ready for pip packaging

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 def versionDev():
     from setuptools_scm.version import get_local_dirty_tag
     def clean_scheme(version):
-        return get_local_dirty_tag(version) if version.dirty else '+clean'
+        return get_local_dirty_tag(version) if version.dirty else ''
 
     return {'local_scheme': clean_scheme}
 
